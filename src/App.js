97587f4780f9cdb369eec837b04723be2aae4e06
giangann <70595717+giangann@users.noreply.github.com>
2022-04-27 14:01:30 +0700

    No title

--- src/App.js
+++ src/App.js
@@ -3,32 +3,16 @@ import "./App.css";
 import ChildOfApp from "./ChildOfApp";
 import React, { useEffect, useState } from "react";
 import DropDown from "./DropDown";
-// import images from "../public"
+import images from "../public";
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [bindNumber, setBindNumber] = useState(number);
-
-  const [count, setCount] = useState(0);
-
-  function importAll(r) {
-    return r.keys().map(r);
-  }
-
-  const images = importAll(
-    require.context("../public/image/Feed_Image", false, /\.(png|jpe?g|svg)$/)
-  );
   console.log(images);
-  
   // why need this dependency?
   // when use API call,
 
-  return (
-    <div>
-      <div>Number: {number} </div>
-      <div>Bind Number: {bindNumber} </div>
-    </div>
-  );
+  return <div>
+    <div>Hello this is first text</div>
+  </div>;
 }
 
 export default App;
